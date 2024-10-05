b7c6eaf1944f06cc9cd6b374f2229fa218a31186
Yazan <yazan.kirresh@hotmail.com>
2024-10-05 14:15:31 +0300

    Removed extra spaces from the function

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,11 @@ function App() {
 
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (input.value.match(validRegex)) {
-  
       alert("Valid email address!");
-  
       document.form1.text1.focus();
-  
-      return true;
-  
     } else {
-  
       alert("Invalid email address!");
-  
       document.form1.text1.focus();
-  
-      return false;
-  
     }
   
   }
